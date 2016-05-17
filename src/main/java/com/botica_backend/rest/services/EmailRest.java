6b9b537a7a6bd74dc5d5cd1ac656c77e8b488c9e
Esteban097 <aymer_el@hotmail.com>
2016-05-17 09:56:00 -0500

    se modifico el route del modulo de recuperacion de conraseña

--- src/main/java/com/botica_backend/rest/services/EmailRest.java
+++ src/main/java/com/botica_backend/rest/services/EmailRest.java
@@ -5,9 +5,16 @@
  */
 package com.botica_backend.rest.services;
 
+import com.botica_backend.entities.Usuario;
 import com.botica_backend.models.RecoveryPassword;
+import com.botica_backend.rest.auth.DigestUtil;
+import com.botica_backend.sessions.UsuarioSession;
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -21,15 +28,29 @@ import org.apache.commons.mail.HtmlEmail;
  * @author Luis
  */
 @Path("email")
+@Stateless
 public class EmailRest {
+
+    @EJB
+    UsuarioSession usuarioSession;
 
     @POST
     @Path("recoverypass")
     @Consumes(MediaType.APPLICATION_JSON)
     public void recoveryPass(RecoveryPassword recoveryPassword) {
         try {
+
+            Usuario user = usuarioSession.findByEmail(recoveryPassword.getEmail());
+            int x = (int) (Math.random() * 1000);
+
+            String cd = null;
+            try {
+                cd = DigestUtil.generateDigest(String.valueOf(x));
+            } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
+                Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
             HtmlEmail email = new HtmlEmail();
-            
 
             email.setHostName("smtp.gmail.com");
             email.setSmtpPort(465);
@@ -37,7 +58,7 @@ public class EmailRest {
             email.setSSL(true);
             email.setFrom("services@botica.com");
             email.setSubject("Restablecer Contraseña Botica");
-            email.setHtmlMsg("<h1 style=" + "color:blue;" + ">Restablecer Contraseña</h1> Email: " + recoveryPassword.getEmail());
+            email.setHtmlMsg("<h1 style=" + "color:blue;" + ">Restablecer Contraseña</h1> Email: " + recoveryPassword.getEmail() + " abra e siguiente enlace para continuar con el proceso de recuperacion es http://localhost:8001/#/passconfrm/" + user.getIdUsuario() + "/" + cd);
             // email.add("djaranzazu@misena.edu.co","maasprilla35@misena.edu.co","hjhenao@misena.edu.co","iyaguirre@misena.edu.co","jcpolanco23@misena.edu.co","aelopez346@misena.edu.co"); //este email es el destinatario, osea el tuyo mismo si queres que te llegen a tu bandeja de entrada
 //            email.addTo("maasprilla35@misena.edu.co");
 //            email.addTo("djaranzazu@misena.edu.co"); //este email es el destinatario, osea el tuyo mismo si queres que te llegen a tu bandeja de entrada
